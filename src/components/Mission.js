9457737b0214d97afc6d6911c63feaeb53d68323
Keshav0148 <keshavagrawal162323@gmail.com>
2025-03-13 20:53:21 +0530

    images added

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "../styles/Mission.css";
+import mission1 from "../images/mission1.png"
 
 const Header = () => {
   return (
-    <header>
+    <header className='header'>
       <h1>Redefining E-Waste - One Device at a Time!</h1>
       <p className="content">
         At TechCycle, we believe in a future where technology and sustainability go hand in hand.
@@ -35,7 +36,7 @@ const MissionSection = () => {
       <h2>Our Mission</h2>
       <div className="mission-container">
         {missions.map((text, index) => (
-          <MissionCard key={index} image="E-waste.png" text={text} />
+          <MissionCard key={index} image={mission1} text={text} />
         ))}
       </div>
     </section>
