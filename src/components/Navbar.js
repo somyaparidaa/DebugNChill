e73ae5ef45bf67a7d5bd4d95120a2213eb83275a
aarnavshreyas <aarnavshreyas2005@gmail.com>
2025-04-05 16:10:44 +0530

    mycommiting2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,11 +2,14 @@
 import React from "react";
 import { Link } from "react-router-dom"; // Import Link from react-router-dom
 import "../styles/Navbar.css";
+import { Recycle } from "lucide-react";
 
 const Header = () => {
   return (
     <header className="nav">
       <div className="logo" id="logo">
+        
+      <Recycle className="logo-icon" />
         TechCycle
       </div>
       <nav>
