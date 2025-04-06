3c739f0396b2c84d01a7b180d1e78399ce5aa464
Keshav0148 <keshavagrawal162323@gmail.com>
2025-04-06 11:38:03 +0530

    env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,12 +5,12 @@ import { getStorage } from "firebase/storage";  // Add this import
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA9FcKGmlnTt-8O1-MpbSAhsv31-7tzQeo",
-  authDomain: "techcycle-afe6b.firebaseapp.com",
-  projectId: "techcycle-afe6b",
-  storageBucket: "techcycle-afe6b.firebasestorage.app",  // This exists in your config
-  messagingSenderId: "19919777135",
-  appId: "1:19919777135:web:0f768792fc08a163120c9a",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
